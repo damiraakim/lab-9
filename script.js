b8da4549a4943a9bd7a516f7ef259f12bbbd6dbb
damiraakim <aslanbek121278@gmail.com>
2024-12-07 11:20:13 +0600

    fourth commit added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const api='c6175bcf171b4b0e9ec172707242511';
-
+ 
 function fetchWeather(city){
     const url = 'https://api.weatherapi.com/v1/current.json?key=' + api + '&q=' + city + '&aqi=no';
     fetch(url)
@@ -18,8 +18,8 @@ function fetchWeather(city){
         showError(error.message);
     })
 }
-
-
+ 
+ 
 function showWeather(data){
     const output=document.getElementById('weather-output');
     const error=document.getElementById('errorMessage');
@@ -28,12 +28,12 @@ function showWeather(data){
     '<div class="city"> Город: '+ data.location.name+'</div>'+ '<div class="weather">Погода: '+ data.current.temp_c+ '</div>'+
     '<div class="weather">Состояние: ' + data.current.condition.text + '</div>';
 }
-
+ 
 function showError(message) {
     const error = document.getElementById('errorMessage');
-    error.textContent = message;  // Теперь 'message' передаётся как параметр
+    error.textContent = message;
 }
-
+ 
 document.getElementById('getWeather').addEventListener('click', function () {
     const city = document.getElementById('cityInput').value.trim();
     if (city) {
@@ -42,19 +42,3 @@ document.getElementById('getWeather').addEventListener('click', function () {
         showError('Введите название города!');
     }
 });
-
-
-// function showError(message){
-//     const output=document.getElementById('weather-output');
-//     const error=document.getElementById('errorMessage');
-//     output.innerHTML=' ';
-//     error.textContent=message;
-// }
-
-// const weatherContainer=document.getElementById('weather-container');
-// const refreshBtn=document.getElementById('refresh-btn');
-// async function fecthWeather(city){
-//     const url = 'https://api.weatherapi.com/v1/current.json?key=' + apiKey + '&q=' + city + '&aqi=no';
-//     const responce=
-
-// }
